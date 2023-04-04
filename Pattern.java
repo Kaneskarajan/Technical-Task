900aabd1d0ed957a74a2049b5099bc59a9872b04
Kaneskarajan <129831954+Kaneskarajan@users.noreply.github.com>
2023-04-04 20:13:34 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+package org.data;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for (int i = 0; i<=5; i++) {
+			if(i==2) {
+				continue;
+			}
+			for (int j = 0; j <=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
